dd118f235c16934bb7ff87df61ae7d19f6ab7da5
kapil-85 <168553846+kapil-85@users.noreply.github.com>
2024-05-15 14:39:00 +0530

    Update MeitYQnA.py

--- MeitYQnA.py
+++ MeitYQnA.py
@@ -112,7 +112,7 @@ st.set_page_config(page_title="Question & Answer", layout="wide")
 #page_bg_img = """
 #<style>
 #[data-testid="stAppViewContainer"]{
-#background-image:  url("https://png.pngtree.com/thumb_back/fh260/background/20190220/ourmid/pngtree-watercolor-light-yellow-light-green-pink-color-image_7008.jpg");
+#background-image:  url("https://img.freepik.com/free-vector/blue-white-gradient-abstract-background_53876-60240.jpg");
 #background-size: cover;
 #}"""
 #st.markdown(page_bg_img, unsafe_allow_html = True)
