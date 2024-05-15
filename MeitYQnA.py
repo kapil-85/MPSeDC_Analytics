197b863e8ce9ba15beb77af463b600794d18b688
kapil-85 <168553846+kapil-85@users.noreply.github.com>
2024-05-15 11:46:21 +0530

    Update MeitYQnA.py

--- MeitYQnA.py
+++ MeitYQnA.py
@@ -95,7 +95,7 @@ def user_input(user_question, api_key):
     #st.write("Reply: ", response["output_text"])
     query = user_question
     response = qa_chain({"question": query, "chat_history": chat_history})
-    st.text_area("##### Reply:", response["answer"],height=200)
+    st.text_area("###### Reply:", response["answer"],height=200)
     #st.code(response["answer"], language="markdown")
     #st.text_area("Reply: ", response["output_text"],height=200)
     
@@ -133,7 +133,7 @@ st.header(" ")
 def main():
     print("================CALL: main START================", datetime.datetime.now())
     with st.form("my_form"):
-        user_question = st.text_input("##### Ask a Question to get references from MeitY RFP guidelines document:", key="user_question", help="Ask your question here...", placeholder="Ask your question here...")
+        user_question = st.text_input("###### Ask a Question to get references from MeitY RFP guidelines document:", key="user_question", help="Ask your question here...", placeholder="Ask your question here...")
         #if user_question and api_key:  # Ensure API key and user question are provided
         submitted = st.form_submit_button("Submit")
         if submitted:
